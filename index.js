b8f2aa0d9dad11736ca178e255aef5200955984e
Matt Condon <mattcondon@google.com>
2015-06-15 13:57:58 -0400

    add optional subdir for all commands

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 module.exports.init = require('./lib/commands/init');
 module.exports.download = require('./lib/commands/download');
 module.exports.upload = require('./lib/commands/upload');
+module.exports.auth = require('./lib/commands/auth');
+module.exports.sync = require('./lib/commands/sync');
+module.exports.config = require('./lib/config');
