b0614a10dd2875923d935e0d8fdbc522015c8aea
Dan Thareja <danthareja@gmail.com>
2015-10-31 16:27:57 -0700

    chore(style) normalize style

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-module.exports.init = require('./lib/commands/init');
-module.exports.download = require('./lib/commands/download');
-module.exports.upload = require('./lib/commands/upload');
 module.exports.auth = require('./lib/commands/auth');
+module.exports.init = require('./lib/commands/init');
+module.exports.upload = require('./lib/commands/upload');
 module.exports.defaults = require('./lib/defaults');
